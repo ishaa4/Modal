776dbebf8094de9848b617e8796cfdad2689a55c
Isha Katyayani <IKatyayani@bpl.netlink.com>
2023-06-01 16:10:00 +0530

    final

--- src/ModalantD.js
+++ src/ModalantD.js
@@ -1,68 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import { Button, Modal, Input, Space } from 'antd';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { Button, Modal } from "antd";
+import folderData from "./data.json";
+import Item from "antd/es/list/Item";
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedType, setSelectedType] = useState('');
-  const [existingNames, setExistingNames] = useState ([]);
-  const [name, setName] = useState();
-
-  useEffect(() =>{
-    fetchNames();
-},[]);
-
-const fetchNames = async () => {
-    const response = await fetch("./data.json").then(
-      (response) => response.json()
-    );
-    const data = existingNames(response);
-    setExistingNames(data.folderName);
-    console.log(existingNames);
-
+  const [selectedType, setSelectedType] = useState("");
+  const [names, setNames] = useState([]);
+  const [nameInput, setNameInput] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [border, setBorder] = useState(false);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      setNames(folderData?.data);
+    } catch (error) {}
   };
-
-  const handlename = (e) => {
-    fetchNames()
-    setName(e.target.value);
-    console.log(name);
-    if (name.trim() === '') {
-        alert('Please enter a name.');
-        return;
-    }
-
-    if(existingNames.includes(name)){
-        alert('Name already exist. Please enter a new Name');
-        return;
-    }
-
-    // console.log(`Submitted Name: ${name}`);
-  
-};
-
-// const handleNameChange = (Event) => {
-//     setName(Event.target.value);
-    
-// };
-
-  const handleTypeChange =(e) => {
-
-    setSelectedType(e.target.value);
-
-
-  }
-
-
+  const handleNameChange = (e) => {
+    setNameInput(e.target.value);
+    setErrorMessage("");
+  };
+  const handleTypeChange = (e) => {
+    setSelectedType(e?.target?.value);
+  };
   const showModal = () => {
     setIsModalOpen(true);
+    fetchData();
   };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
   const handleCancel = () => {
     setIsModalOpen(false);
+  };
+
+  const handleSubmit = () => {
+    if (names?.map((e) => e.folderName)?.includes(nameInput)) {
+      setErrorMessage("Name already exists");
+      setBorder(true);
+    } else {
+      setErrorMessage("checked");
+      setBorder(false);
+    }
   };
 
   return (
@@ -70,53 +49,92 @@ const fetchNames = async () => {
       <Button type="primary" onClick={showModal}>
         Open Modal
       </Button>
-      <Modal title="Create New Training" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal
+        title="Create New Training"
+        open={isModalOpen}
+        onOk={handleSubmit}
+        onCancel={handleCancel}
+      >
         <div>
-        <p>Please provide below information to create a training:</p> 
-        <div>
-        <label>Training Name:</label> <br/>
-        <input type="text" id= "name" placeholder="Please enter a unique name" value={name} onChange={handlename}/><br/> <br/>
-        </div>
-        <div>
-        <label >Event Type:</label> <br/>
-        <div>
+          <p>Please provide below information to create a training:</p>
           <div>
-        <label>
-        <input type="radio" name="type" value="call" checked= {selectedType === 'call'} onChange={handleTypeChange} />
-        Call Type
-        </label>
-        {selectedType === 'call' && (
-          <div> 
-            <label>
-            <input type="radio" name="callSubType" value="landline" />
-            Landline </label>
-            <label>
-            <input type="radio" name="callSubType" value="telematics" />
-            Telematics </label>
+            <label>Training Name:</label> <br />
+            <input
+              type="text"
+              id="names"
+              placeholder="Please enter a unique name"
+              value={nameInput}
+              onChange={handleNameChange}
+              style={border ? { border: "2px solid red" } : { border: "" }}
+            />
+            <br /> <br />
+            {errorMessage}
+          </div>
+          <div>
+            <label>Event Type:</label> <br />
+            <div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="type"
+                    value="call"
+                    checked={selectedType === "call"}
+                    onChange={handleTypeChange}
+                  />
+                  Call Type
+                </label>
+                {selectedType === "call" && (
+                  <div>
+                    <label>
+                      <input type="radio" name="callSubType" value="landline" />
+                      Landline{" "}
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="callSubType"
+                        value="telematics"
+                      />
+                      Telematics{" "}
+                    </label>
+                  </div>
+                )}
+              </div>
+              <div>
+                <label>
+                  <input
+                    type="radio"
+                    name="type"
+                    value="event"
+                    checked={selectedType === "event"}
+                    onChange={handleTypeChange}
+                  />
+                  Event Type
+                </label>
+                {selectedType === "event" && (
+                  <div>
+                    <label>
+                      <input type="radio" name="eventSubType" value="svl" />
+                      SVL{" "}
+                    </label>
+                    <label>
+                      <input type="radio" name="eventSubType" value="acn" />
+                      ACN{" "}
+                    </label>
+                    <label>
+                      <input
+                        type="radio"
+                        name="eventSubType"
+                        value="followup"
+                      />
+                      Follow Up{" "}
+                    </label>
+                  </div>
+                )}
+              </div>
             </div>
-        )}
-        </div>
-        <div>
-          <label>
-          <input type="radio" name="type" value="event" checked= {selectedType === 'event'} onChange={handleTypeChange} />
-        Event Type
-        </label>
-        {selectedType === 'event' && (
-          <div> 
-            <label>
-            <input type="radio" name="eventSubType" value="svl" />
-            SVL </label>
-            <label>
-            <input type="radio" name="eventSubType" value="acn" />
-            ACN </label>
-            <label>
-            <input type="radio" name="eventSubType" value="followup" />
-            Follow Up </label>
-            </div>
-        )}
-        </div> 
-        </div>
-        </div>
+          </div>
         </div>
       </Modal>
     </>
